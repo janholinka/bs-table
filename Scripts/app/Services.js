ca629d7480eb61cac406b452250eefd626758b2e
Jan Holinka <jan.holinka@outlook.com>
2016-02-26 21:55:05 +0100

    Updated bootswatch source url

--- Scripts/app/Services.js
+++ Scripts/app/Services.js
@@ -2,7 +2,7 @@
 
 /* services module */
 
-var bootsWatchApi = "http://api.bootswatch.com/3/";
+var bootsWatchApi = "http://bootswatch.com/api/3.json";
 
 angular.module("BsTableDirective.Services", [])
     .factory("BootswatchService", ["$http", function ($http) {
