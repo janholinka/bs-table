d7f7e58f28d4faa52a6c8286534d3db17fda7867
Jan Holinka <jan.holinka@outlook.com>
2017-10-31 10:15:22 +0100

    Fixed bootswatch url

--- Scripts/app/Services.js
+++ Scripts/app/Services.js
@@ -2,7 +2,7 @@
 
 /* services module */
 
-var bootsWatchApi = "http://bootswatch.com/api/3.json";
+var bootsWatchApi = "https://bootswatch.com/api/3.json";
 
 angular.module("BsTableDirective.Services", [])
     .factory("BootswatchService", ["$http", function ($http) {
